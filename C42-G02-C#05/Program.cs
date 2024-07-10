2f6169ca92cead8d1bb45f3dcddab1b42185f159
mali84211 <mali84211@gmail.com>
2024-07-10 20:11:39 +0300

    Q1

--- C42-G02-C#05/Program.cs
+++ C42-G02-C#05/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1
+            //Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
+            int valueType = 10;
+            ModifyValueType(valueType);
+            Console.WriteLine("After ModifyValueType: " + valueType); // Output: 10
+
+            ModifyValueTypeByRef(ref valueType);
+            Console.WriteLine("After ModifyValueTypeByRef: " + valueType); // Output: 20
+
+            // Passing by value
+            static void ModifyValueType(int value)
+            {
+                value = 20;
+            }
+
+            // Passing by reference
+            static void ModifyValueTypeByRef(ref int value)
+            {
+                value = 20;
+            }
+            #endregion
         }
     }
 }
