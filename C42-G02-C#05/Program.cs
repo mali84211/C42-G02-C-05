b54b3704432f15b6667ef15d8ca56710c3a3e717
mali84211 <mali84211@gmail.com>
2024-07-10 20:36:28 +0300

    boxing, unboxing

--- C42-G02-C#05/Program.cs
+++ C42-G02-C#05/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region V2
+            object O1;
+            O1 = "Ahmed"; //casting
+            O1 = 1;
+            O1 = "A";
+            O1 = new Employee();
+            #region
+            int x = 10;
+            object obj = x;
+            obj= new object(10);
+            object obj2 = 10;
+            int y =(int)obj2;
+            Console.WriteLine(y);
+            #endregion
+            #endregion
         }
     }
 }
